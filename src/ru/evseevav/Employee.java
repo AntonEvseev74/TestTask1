3aa1a19458730b852613525731ad151fd282184f
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2020-11-08 13:36:07 +0500

    Исправлен класс Vacancy

--- src/ru/evseevav/Employee.java
+++ src/ru/evseevav/Employee.java
@@ -4,14 +4,21 @@ public class Employee {
     private long id;
     private Vacancy vacancy;
 
-    /* Добавленный код */
+    /* Добавленный код, начало */
     /* Конструктор класса */
     public Employee(long id, String vacancy) {
         this.id = id;
         this.vacancy = new Vacancy(vacancy);
+        // при создании объекта класса Vacancy передается параметр
+        // в самом классе Vacancy отсутствует необходимый конструктор, нужно создать
+        // а так же отсутствует поле принимаемое значение передаваемого парметра
+        // Переходим в класс Vacancy:
+        // добавляем поле:=> String vacancy;
+        // добавляем конструктор:=> Vacancy (String vacancy) {this.vacancy = vacancy;}
     }
     /* При необходимости создания пустого объекта можно добавить пустой конструктор класса */
     public Employee(){}
+    /* Добавленный код, конец */
 
     String getVacancy() {
         return vacancy.getVacancy();
--- src/ru/evseevav/Vacancy.java
+++ src/ru/evseevav/Vacancy.java
@@ -1,6 +1,16 @@
 package ru.evseevav;
 
 public class Vacancy {
+    /* Добавленный код, начало */
+    /* добавляем поле vacancy типа String */
+    String vacancy;
+
+    /* Добавляем конструктор с параметром */
+    Vacancy (String vacancy) {
+        this.vacancy = vacancy;
+    }
+    /* Добавленный код, конец. Переходим в класс Main и проверяем работу программы */
+
     String getVacancy() {
         return "engineer";
     }
