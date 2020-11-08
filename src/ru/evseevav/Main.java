779d3e833f8e875a3ebaa7f72bde944784408434
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2020-11-08 13:48:02 +0500

    Задача решена. Ввод: 7, Developer Вывод: 7 Developer

--- src/ru/evseevav/Main.java
+++ src/ru/evseevav/Main.java
@@ -3,12 +3,19 @@ package ru.evseevav;
 public class Main {
 
     public static void main(String[] args) {
+        /* Ввод (7, Developer) */
 	    Employee tester = new Employee(7, "Developer");
 	    // Объект tester создается через конструктор класса Employee с аргументами (long, String)
 	    // В классе Employee отстутствует необходимый конструктор
         // Нужно написать в классе Employee конструктор с аргументами (long, String)
 
+        /* Вывод:
+            7
+            Developer
+         */
         System.out.println(tester.getId());
         System.out.println(tester.getVacancy());
     }
 }
+
+/* Задача решена */
