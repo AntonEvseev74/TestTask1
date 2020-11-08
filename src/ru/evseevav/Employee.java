58fd62ebb4a0b5c00704200262bc09cb8cbdd8c4
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2020-11-08 13:27:39 +0500

    Созданы классы соответствующие данным в условии задачи классам.

--- src/ru/evseevav/Employee.java
+++ src/ru/evseevav/Employee.java
@@ -4,6 +4,17 @@ public class Employee {
     private long id;
     private Vacancy vacancy;
 
+    /* Добавленный код */
+    /* Конструктор класса */
+    public Employee(long id, String vacancy) {
+        this.id = id;
+        this.vacancy = new Vacancy(vacancy);
+        // при создании объекта класса Vacancy передается параметр
+        // в самом классе Vacancy отсутствует необходимый конструктор, нужно создать
+    }
+    /* При необходимости создания пустого объекта можно добавить пустой конструктор класса */
+    public Employee(){}
+
     String getVacancy() {
         return vacancy.getVacancy();
     }
--- src/ru/evseevav/Main.java
+++ src/ru/evseevav/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    Employee tester = new Employee(7, "Developer");
+	    // Объект tester создается через конструктор класса Employee с аргументами (long, String)
+	    // В классе Employee отстутствует необходимый конструктор
+        // Нужно написать в классе Employee конструктор с аргументами (long, String)
+
         System.out.println(tester.getId());
         System.out.println(tester.getVacancy());
     }
